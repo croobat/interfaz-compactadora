e2845cea35f5587524f4eeb54d83554de156426a
Tony Ramírez <tonyram9906@outlook.com>
2022-04-03 00:17:11 -0600

    agregada logica de estado en inactivo (monitor serial)

--- src/main/eIdle.ino
+++ src/main/eIdle.ino
@@ -1,13 +1,32 @@
 //==================< Idle >=====================
 void stateIdle(){
     Serial.println("Inactivo");
-    // Actualizar peso
+
+    /* ---------------------- */
+    /* -  Actualizar peso  - */
+    /* ---------------------- */
+
+
+        /* ---------------------- */
+        /* -  test  - */
+        /*  ---------------------- */
+        Serial.println("Inactivo");
+        while (Serial.available() == 0) {}
+        accion = Serial.readStringUntil('\n');
+        accion.trim();
+
 }
 
 //-----< De Inactivo a Rellenar >-----
 bool transitionIdleFill(){
-    if (false) {
+    if (accion == opendoor) {
         // Sensor magnético abierto
+
+        /* ---------------------- */
+        /* -  test  - */
+        /*  ---------------------- */
+        Serial.println("Accion: " + accion + " | hacia rellenar");
+
         return true;
     }
     return false;
@@ -15,8 +34,14 @@ bool transitionIdleFill(){
 
 //-----< De Inactivo a Reiniciar >-----
 bool transitionIdleReset(){
-    if (false) {
+    if (accion == offswitch) {
         // Interruptor de apagado
+
+        /* ---------------------- */
+        /* -  test  - */
+        /*  ---------------------- */
+        Serial.println("Accion: " + accion + " | hacia reiniciar");
+
         return true;
     }
     return false;
