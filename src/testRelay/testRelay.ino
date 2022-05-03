df36b50df6713a5a5fc45c70e696d6a30eb6ce60
Tony Ramirez <tonyram9906@outlook.com>
2022-05-02 19:53:10 -0500

    added relay test

---
+++ src/testRelay/testRelay.ino
@@ -0,0 +1,16 @@
+// constants won't change
+const int RELAY_PIN = 3;  // the Arduino pin, which connects to the IN pin of relay
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin as an output.
+  pinMode(RELAY_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(RELAY_PIN, HIGH);
+  delay(500);
+  digitalWrite(RELAY_PIN, LOW);
+  delay(500);
+}`
